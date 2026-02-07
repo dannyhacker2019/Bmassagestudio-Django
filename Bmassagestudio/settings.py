aeb6afb27879b46f295780612ec60f183ccb258e
danny <dannyhacker2019@gmail.com>
2026-02-07 02:40:29 -0700

    Edit railway link to allowed hosts

--- Bmassagestudio/settings.py
+++ Bmassagestudio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-m&(*d-m%(gk3c#60r=@p#5ck_c8eggitnb8@bp3x$yqn4fotx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
